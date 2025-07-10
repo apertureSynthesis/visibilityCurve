2885ee875b5a7ece3e9cad1804857203c46353cf
apertureSynthesis <160927686+apertureSynthesis@users.noreply.github.com>
2025-07-10 16:10:47 -0400

    Adding documentation

--- visibility_curve.py
+++ visibility_curve.py
@@ -5,6 +5,26 @@ from visibilityCurve.vis_sample import import_data_ms
 
 class visibilityCurve(object):
 
+    """
+    A class that plots a visibility curve from an observed ALMA comet Measurement Set
+    along with parent and daughter models, also stored as Measurement Sets.
+
+    Inputs:
+    obsVis: observed ALMA Measurement Set
+    parentVis: parent (Haser) model Measurement Set
+    daughterVis: daughter (Haser) model Measurement Set
+    binWid: binning width (in meters) for uv-distance
+    binoffset: offset the binning from 0 if desired (m)
+    lpString: string describing the label for the daughter model (presumably Lp)
+    titleString: string for the title of the plot
+    figName: string for the name of the output plot file. must include desired file extension
+    tp: optionally add total power data and model (TBD)
+    tpe: optionally add errors on total power data and model (TBD)
+    uvmax: optionally cut off visibilities at a maximum uv-distance (m)
+
+    Outputs:
+    a plot showing the observed and modeled binned visibilities with labels
+    """
     def __init__(self,obsVis,parentVis,daughterVis,binWid,binOffset,lpString,titleString,figName,tp=None,tpe=None,uvmax=None):
         self.obsVis = obsVis
         self.parentVis = parentVis
@@ -22,14 +42,19 @@ class visibilityCurve(object):
         
 
     def import_vis(self,file):
+        #Read in a measurement set using Ryan Loomis' vis_sample
         vis = import_data_ms(file)
         self.f0 = 0.5 * (vis.freqs[-1] + vis.freqs[0])[0]
         self.dv = self.c * np.abs((vis.freqs[1] - vis.freqs[0])/self.f0) / 1000.
+        #Real part of the visibilities
         re = np.squeeze(np.real(vis.VV))
+        #Measured noise on visibilities
         visRMS = np.nanstd(re)
         rms = np.zeros(len(re))+visRMS
+        #uv-distances
         uv = np.sqrt(vis.uu**2 + vis.vv**2) * self.c / self.f0
 
+        #Cut off at a maximum uv-distance if desired
         if self.uvmax != None:
             uvInds = np.where(uv <= self.uvmax)
             uv = uv[uvInds]
@@ -39,6 +64,7 @@ class visibilityCurve(object):
         return uv, re, rms
 
     def binVis(self, uv, re, rms, chans):
+        #Bin the visibilities
         bins = np.arange(self.binOffset, uv.max(), self.binWid)
         binInds = np.digitize(uv,bins)
 
@@ -80,15 +106,17 @@ class visibilityCurve(object):
         pCurve = interp1d(puvBinned,preBinned,kind='cubic',fill_value='extrapolate',bounds_error=False)
         dCurve = interp1d(duvBinned,dreBinned,kind='cubic',fill_value='extrapolate',bounds_error=False)
 
+        #Finer grid of baselines for plotting the interpolated models
         uvInterp = np.arange(np.min(uvBinned),np.max(uvBinned),(np.max(uvBinned)-np.min(uvBinned))/100.)
-        uvsInterp = [i / (1e3*self.c/(self.f0)) for i in uvInterp]
 
         pInterp = pCurve(uvInterp)
         dInterp = dCurve(uvInterp)
 
         #Generate baselines in kilo-lambda for plotting
         uvsBinned = [i / (1e3*self.c/(self.f0)) for i in uvBinned]
+        uvsInterp = [i / (1e3*self.c/(self.f0)) for i in uvInterp]
 
+        #Integrate everything
         reBinned *= self.dv * len(datachans)
         yerrs *= self.dv * len(datachans)
         preBinned *= self.dv * len(datachans)
@@ -96,6 +124,7 @@ class visibilityCurve(object):
         pInterp *= self.dv * len(datachans)
         dInterp *= self.dv * len(datachans)
 
+        #Make the figure
         fig, ax = plt.subplots(figsize=(10,10))
 
         ax.errorbar(uvsBinned, reBinned, yerr=yerrs, linestyle=' ',marker='o',markersize=8,elinewidth=1.5,ecolor='k',
@@ -111,7 +140,7 @@ class visibilityCurve(object):
         ax.set_ylabel('Re(Visibility) (Jy km s$^{-1}$)',fontsize=25)
         ax.set_title(self.titleString,fontsize=25,fontweight='bold')
 
-        #ax.legend()
+        #Order the legend items
         h,l = ax.get_legend_handles_labels()
         h = [h[2],h[0],h[1]]
         l = [l[2],l[0],l[1]]
